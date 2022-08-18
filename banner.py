600c1f7421766a955fe67c080f929517278e152f
Juan Carlos Bindez <juanbindez780@gmail.com>
2022-08-18 12:31:56 -0300

    Update banner.py

--- banner.py
+++ banner.py
@@ -11,17 +11,17 @@ def header_banner():
                 '''
                                       ?B^  :55555PPPPPPPPPGG:JGGGGGGBBBBBBBBBP                 
                                   ~PGGJ777~:~!!!!!~^~777777!.^7?????~~7JJJJJ?:                 
-                               .YBP5PB####G5 YYYYYY.755555PPPPPPPGGG?:GGGGGGBY                 
+                               .YBP5PB####G5 YYYYYY. 755555PPPPPPPGGG? GGGGGGBY                 
                            :?~.:GBP5Y5PB###BP 555555:JP5PPPPPPGGGGGGB5~BBBBBBBP                 
                             JBGGBGPY7J5PG###BP 55555.?PPPPPPGGGGGGGGBY^BBBBBB#P                 
                             ^BGY5GPY7~?Y5GB##BY ?!!~.:!77777!^~7?????^.7JJJJJ?:                 
-                            !B5JJYYJ!~~7J5GGB#BP5 YYYYYY55555.7PPPPPGGGGGGGGGBY                 
+                            !B5JJYYJ!~~7J5GGB#BP5 YYYYYY55555 7PPPPPGGGGGGGGGBY                 
                             JGJ????7^:^^!?5GGBB#BP 555555PPPG^YGGGGGGGGBBBBBBBG                 
                             YP7777~:..:::~JPP5B#BP 55555PPPPG:JGGGGGGBBBBBBBB#P                 
-                            !5!!!^......::~7!7G#J! ~^^!!7777!.^7?????~~7???JJ?:                 
+                            !5!!!^......::~7!7G#J! ~^^!!7777! ^7?????~~7???JJ?:                 
                              Y7~^:........^~~7BB 5YY.!Y55555PPPPPPPPG7.PGGGGGGY                 
                              .7~:. .......::^5BP 555:J55PPPPPGGGGGGGBY^BGGBBBBP                 
-                               ::.       .:~PBP 55PP:JPPPPGGGGGGGGBBBY^BBBBBB#P   
+                               ::.       .:~PBP 55PP JPPPPGGGGGGGGBBBY BBBBBB#P   
                                          _____ _                        _ _ 
                              _ __  _   _|  ___(_)_ __ _____      ____ _| | |
                             | '_ \| | | | |_  | | '__/ _ \ \ /\ / / _` | | |
