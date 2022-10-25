54bf298099930a42720adbc9b4bf215b589c1eb9
Juan Carlos Bindez <juanbindez780@gmail.com>
2022-10-25 10:35:28 -0300

    2510221035

--- ipv4/logica_ipv4.py
+++ ipv4/logica_ipv4.py
@@ -1,5 +1,7 @@
 # A interface of the iptables written in Python.
 #
+#     pyIptables-v1.3.0/ipv4/logica_ipv4.py
+#
 # Copyright (c) 2022  Juan Carlos Bindez
 #
 # This program is free software; you can redistribute it and/or modify
@@ -16,8 +18,8 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #  
-# autor: https://github.com/juanBindez
-# e-mail: juanbindez780@gmail.com
+# autor: https://github.com/juanBindez   <juanbindez780@gmail.com>
+
 
 
 import os
@@ -25,84 +27,81 @@ from colors import Color
 
 
 class LogicasMenu1:
-    '''
-           Estes metodos executam comandos do os.system().
-    '''
+    '''Estes metodos executam comandos do os.system().'''
+    
     def __init__(self, command):
         self.command = command
 
 
-    # Start em comandos.
     def start_command(self):
+        ''' Start em comandos.'''
+        
         os.system(self.command)
 
-
-    # Deleta regra de tabela especifica no iptables.
+ 
     def delete_id(self):
+        '''Deleta regra de tabela especifica no iptables.'''
+        
         id = int(input(Color.VERMELHO + " digite numero da regra a ser deletada \n>>"))
         os.system(self.command.format(id))
 
 
-    # Altera porta especifica no iptables.
+    
     def port_change(self):
+        '''Altera porta especifica no iptables.'''
+        
         port = str(input(Color.VERMELHO + "Digite a Porta Escolhida \n PORT >> "))
         os.system(self.command.format(port))
 
 
-    # Executa comandos de regras de ip.
+    
     def ip_func_regra(self):
+        ''' Executa comandos de regras de ip.'''
+        
         ip = str(input(Color.VERMELHO + "Digite o ip Escolhido \n IP >> "))
         os.system(self.command.format(ip))
 
-    # Executa comandos de regras de Mac.
+    
     def mac_func_regra(self):
+        '''Executa comandos de regras de Mac.'''
+        
         mac = str(input(Color.VERMELHO + "Digite o MacAddress Escolhido \n Mac >> "))
         os.system(self.command.format(mac))
 
 
 class RegrasList:
-    '''
-        Aqui esta os comandos de iptables para regra de firewall,
-        para desbloqueio de portas.
-    '''
+    ''' Aqui esta os comandos de iptables para regra de firewall, para desbloqueio de portas.'''
+
     ports_tab_input_accept = LogicasMenu1("sudo iptables -A INPUT -p tcp --dport {} -j ACCEPT")
     ports_tab_forward_accept = LogicasMenu1("sudo iptables -A FORWARD -p tcp --dport {} -j ACCEPT")
     ports_tab_output_accept = LogicasMenu1("sudo iptables -A OUTPUT -p tcp --dport {} -j ACCEPT")
 
-    '''
-       Aqui esta os comandos de iptables para regra de firewall,
-       para bloqueio de portas.
-    '''
+
+    # Aqui esta os comandos de iptables para regra de firewall, para bloqueio de portas.
     ports_tab_input_drop = LogicasMenu1("sudo iptables -A INPUT -p tcp --dport {} -j DROP")
     ports_tab_forward_drop = LogicasMenu1("sudo iptables -A FORWARD -p tcp --dport {} -j DROP")
     ports_tab_output_drop = LogicasMenu1("sudo iptables -A OUTPUT -p tcp --dport {} -j DROP")
 
 
 class DeleteRegra:
-    '''
-        Aqui os comandos do iptables para deletar regras.
+    '''Aqui os comandos do iptables para deletar regras.'''
     
-    '''
     delete_INPUT = LogicasMenu1("sudo iptables -D INPUT {}")
     delete_FORWARD = LogicasMenu1("sudo iptables -D FORWARD {}")
     delete_OUTPUT = LogicasMenu1("sudo iptables -D OUTPUT {}")
 
 
 class SaveTable:
-    '''
-        Aqui os comandos iptables para ver o status do serviço iptables,
-        restartar o serviço e iniciar o serviço.
+    '''Aqui os comandos iptables para ver o status do serviço iptables, restartar o serviço e iniciar o serviço.'''
     
-    '''
     status_service = LogicasMenu1("sudo systemctl status netfilter-persistent.service")
     status_service = LogicasMenu1("sudo systemctl start netfilter-persistent.service")
     status_service = LogicasMenu1("sudo systemctl restart netfilter-persistent.service")
     
 
 class IpRegras:
-    '''
-        Aqui as regras especificas para ip
-    '''
+    '''Aqui as regras especificas para ip'''
+
     ip_ACCEPT_tab_INPUT = LogicasMenu1("sudo iptables -A INPUT -s {} -j ACCEPT")
     ip_DROP_tab_INPUT = LogicasMenu1("sudo iptables -A INPUT -s {} -j DROP")
 
@@ -114,9 +113,8 @@ class IpRegras:
 
 
 class MacRegras:
-    '''
-        Aqui as regras especificas para Mac
-    '''
+    '''Aqui as regras especificas para Mac'''
+
     mac_ACCEPT_tab_INPUT = LogicasMenu1("sudo iptables -A INPUT -m mac --mac-source {} -j ACCEPT")
     mac_DROP_tab_INPUT = LogicasMenu1("sudo iptables -A INPUT -m mac --mac-source {} -j DROP")
 
