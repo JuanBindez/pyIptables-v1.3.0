6bd69c62f0567631770fe16db1a2264871c3a70e
Juan Carlos Bindez <juanbindez780@gmail.com>
2023-01-02 21:00:35 -0300

    0201232100

--- pyiptables.py
+++ pyiptables.py
@@ -2,7 +2,7 @@
 #
 # pyIptables-v1.3.0/pyiptables.py
 #
-# Copyright (c) 2022  Juan Carlos Bindez
+# Copyright (c) 2022-2023  Juan Carlos Bindez
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
