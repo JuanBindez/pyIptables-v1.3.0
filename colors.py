4414857343a5937545f21b3bafef207ea82326f1
Juan Carlos Bindez <juanbindez780@gmail.com>
2023-01-02 21:03:22 -0300

    Update colors.py

--- colors.py
+++ colors.py
@@ -1,7 +1,7 @@
 '''
 MIT License
 
-Copyright (c) 2022 Juan Carlos Bindez
+Copyright (c) 2022-2023 Juan Carlos Bindez
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
