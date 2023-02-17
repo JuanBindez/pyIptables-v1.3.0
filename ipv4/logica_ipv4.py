065892d42536472e83adeeba23696247e2b8527f
Juan Bindez <juanbindez780@gmail.com>
2023-02-16 23:12:17 -0300

    1602232312

--- ipv4/logica_ipv4.py
+++ ipv4/logica_ipv4.py
@@ -21,7 +21,6 @@
 # autor: https://github.com/juanBindez   <juanbindez780@gmail.com>
 
 
-
 import os
 from colors import Color
 
@@ -32,27 +31,22 @@ class LogicasMenu1:
     def __init__(self, command):
         self.command = command
 
-
     def start_command(self):
         ''' Start em comandos.'''
         
         os.system(self.command)
 
- 
     def delete_id(self):
         '''Deleta regra de tabela especifica no iptables.'''
         
         id = int(input(Color.VERMELHO + " digite numero da regra a ser deletada \n>>"))
         os.system(self.command.format(id))
 
-
-    
     def port_change(self):
         '''Altera porta especifica no iptables.'''
         
         port = str(input(Color.VERMELHO + "Digite a Porta Escolhida \n PORT >> "))
         os.system(self.command.format(port))
-
 
     
     def ip_func_regra(self):
@@ -61,21 +55,19 @@ class LogicasMenu1:
         ip = str(input(Color.VERMELHO + "Digite o ip Escolhido \n IP >> "))
         os.system(self.command.format(ip))
 
-    
     def mac_func_regra(self):
         '''Executa comandos de regras de Mac.'''
         
         mac = str(input(Color.VERMELHO + "Digite o MacAddress Escolhido \n Mac >> "))
         os.system(self.command.format(mac))
 
-
+        
 class RegrasList:
     ''' Aqui esta os comandos de iptables para regra de firewall, para desbloqueio de portas.'''
 
     ports_tab_input_accept = LogicasMenu1("sudo iptables -A INPUT -p tcp --dport {} -j ACCEPT")
     ports_tab_forward_accept = LogicasMenu1("sudo iptables -A FORWARD -p tcp --dport {} -j ACCEPT")
     ports_tab_output_accept = LogicasMenu1("sudo iptables -A OUTPUT -p tcp --dport {} -j ACCEPT")
-
 
     # Aqui esta os comandos de iptables para regra de firewall, para bloqueio de portas.
     ports_tab_input_drop = LogicasMenu1("sudo iptables -A INPUT -p tcp --dport {} -j DROP")
@@ -90,7 +82,6 @@ class DeleteRegra:
     delete_FORWARD = LogicasMenu1("sudo iptables -D FORWARD {}")
     delete_OUTPUT = LogicasMenu1("sudo iptables -D OUTPUT {}")
 
-
 class SaveTable:
     '''Aqui os comandos iptables para ver o status do serviço iptables, restartar o serviço e iniciar o serviço.'''
     
@@ -98,7 +89,6 @@ class SaveTable:
     status_service = LogicasMenu1("sudo systemctl start netfilter-persistent.service")
     status_service = LogicasMenu1("sudo systemctl restart netfilter-persistent.service")
     
-
 class IpRegras:
     '''Aqui as regras especificas para ip'''
 
@@ -110,7 +100,6 @@ class IpRegras:
 
     ip_ACCEPT_tab_OUTPUT = LogicasMenu1("sudo iptables -A OUTPUT -s {} -j ACCEPT")
     ip_DROP_tab_OUTPUT = LogicasMenu1("sudo iptables -A OUTPUT -s {} -j DROP")
-
 
 class MacRegras:
     '''Aqui as regras especificas para Mac'''
